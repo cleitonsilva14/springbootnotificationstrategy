1c977973bf0bcd00c8977bd46cf8bef893f41f2c
cleitonsilva14 <cleiton18ads@gmail.com>
2026-02-13 11:33:46 -0300

    Adicionada validação para validar valores nullos

--- src/main/java/br/com/springbootnotificationstrategy/service/NotificationService.java
+++ src/main/java/br/com/springbootnotificationstrategy/service/NotificationService.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import br.com.springbootnotificationstrategy.handler.exception.NotificationChannelNotFound;
 import br.com.springbootnotificationstrategy.service.strategy.DiscordNotificationStrategy;
 import br.com.springbootnotificationstrategy.service.strategy.RedditNotificationStrategy;
 import br.com.springbootnotificationstrategy.service.strategy.TelegramNotificationStrategy;
+import br.com.springbootnotificationstrategy.service.strategy.WhatsappNotificationStrategy;
 
 @Service
 public class NotificationService {
@@ -16,22 +18,21 @@ public class NotificationService {
 	private final Map<String, NotificationStrategy> strategies = Map.of(
 			"discord", new DiscordNotificationStrategy(),
 			"telegram", new TelegramNotificationStrategy(),
-			"reddit", new RedditNotificationStrategy()
+			"reddit", new RedditNotificationStrategy(),
+			"whatsapp", new WhatsappNotificationStrategy()
 		);
 	
 	public void notify(String channel, String destination, String message) {
 		
 		// strategies.get(channel).sendNotification(destination, message);
 		
+		
 		Optional.ofNullable(strategies.get(channel))
 		.ifPresentOrElse(
 				strategy -> strategy.sendNotification(destination, message), 
 				() -> {
 					//System.out.println("\033[31mCanal de notificação: %s inválido\033[m".formatted(channel));
-					throw new IllegalArgumentException("Canal de notificação: %s inválido".formatted(channel));
-				}
-				);
-		
+					throw new NotificationChannelNotFound("Canal de notificação: %s inválido".formatted(channel));
+				});
 	}
-	
 }
